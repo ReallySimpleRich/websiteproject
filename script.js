46018dfda70144faac918dea3a83f044183c4ab7
ReallySimpleRich <133661976+ReallySimpleRich@users.noreply.github.com>
2023-05-16 07:23:29 +0700

    Updated files

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 function goToBooking() {
     window.location.assign("bookingform.html");
-}
-
-function process() {
-    var allPackage = document.getElementById("package");
-    var package = allPackage.value;
-    var num_of_people = document.getElementById("num-of-people");
-    var totalPayment;
-    if(package === "pack1"){
-        totalPayment = 3500 * num_of_people;
-    } else if(package === "pack2"){
-        totalPayment = 2500 *num_of_people;
-    } else if(package === "pack3"){
-        totalPayment = 9000 * num_of_people;
-    } else if(package === "pack4"){
-        totalPayment = 4500 * num_of_people;
-    } else if(package === "pack5"){
-        totalPayment = 5500 * num_of_people;
     }
 
+function process() {
+    var allpackage = document.getElementById("package");
+    var package = allpackage.value;
+    var num_of_people = document.getElementById("num-of-people").value
+    var totalPayment;
+    if (package === "pack1") {
+        totalPayment = 1500 * num_of_people;
+    }   else if (package === "pack2") {
+        totalPayment = 3500 * num_of_people;
+    }   else if (package === "pack3") {
+        totalPayment = 6000 * num_of_people;
+    }   else if (package === "pack4") {
+        totalPayment = 10000 * num_of_people;
+    }   else if (package === "pack5") {
+        totalPayment = 15000 * num_of_people;
+    }   
     document.getElementById('total').innerHTML = totalPayment;
-    setTimeout(() => {
-        alert('Thank You, the booking is succesful')
-        window.location.assign('about.html')
+setTimeout(() => {
+    alert('Thank you! Your booking is a Succes')
+    window.location.assign('about.html')
     }, 1000);
 }
